d6ccef7f3fe133a2a97d036cac2d5257545dcc54
Julio López Torres <juliolopeztorres@gmail.com>
2017-12-18 10:21:50 +0100

    feat: Define models for /champion endpoint

---
+++ app/src/main/java/oob/lolprofile/HomeComponent/Domain/Model/Champion.java
@@ -0,0 +1,66 @@
+package oob.lolprofile.HomeComponent.Domain.Model;
+
+import java.util.ArrayList;
+
+public class Champion {
+    private int id;
+    private String name;
+    private String title;
+    private String lore;
+    private String image;
+    private ArrayList<Skin> skins;
+
+    public int getId() {
+        return id;
+    }
+
+    public Champion setId(int id) {
+        this.id = id;
+        return this;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Champion setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public Champion setTitle(String title) {
+        this.title = title;
+        return this;
+    }
+
+    public String getLore() {
+        return lore;
+    }
+
+    public Champion setLore(String lore) {
+        this.lore = lore;
+        return this;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public Champion setImage(String image) {
+        this.image = image;
+        return this;
+    }
+
+    public ArrayList<Skin> getSkins() {
+        return skins;
+    }
+
+    public Champion setSkins(ArrayList<Skin> skins) {
+        this.skins = skins;
+        return this;
+    }
+}
