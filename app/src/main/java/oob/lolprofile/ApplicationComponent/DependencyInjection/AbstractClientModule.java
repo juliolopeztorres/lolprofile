311d1172b169407741d92f52216c98cf97723b71
Julio López Torres <juliolopeztorres@gmail.com>
2018-08-20 19:43:05 +0200

    feat: Add Backend timeout to 60 seconds as wold-wide requests are taking too long

--- app/src/main/java/oob/lolprofile/ApplicationComponent/DependencyInjection/AbstractClientModule.java
+++ app/src/main/java/oob/lolprofile/ApplicationComponent/DependencyInjection/AbstractClientModule.java
@@ -1,5 +1,7 @@
 package oob.lolprofile.ApplicationComponent.DependencyInjection;
 
+import java.util.concurrent.TimeUnit;
+
 import dagger.Module;
 import dagger.Provides;
 import okhttp3.Cache;
@@ -8,12 +10,17 @@ import okhttp3.logging.HttpLoggingInterceptor;
 
 @Module(includes = {LogModule.class, CacheModule.class})
 public class AbstractClientModule {
+    private static final int BACKEND_TIMEOUT = 60;
+
     @Provides
     @BaseApplicationScopeInterface
     OkHttpClient provideOkHttpClient(HttpLoggingInterceptor httpLoggingInterceptor, Cache cache) {
         return new OkHttpClient.Builder()
                 .addInterceptor(httpLoggingInterceptor)
                 .cache(cache)
+                .connectTimeout(BACKEND_TIMEOUT, TimeUnit.SECONDS)
+                .readTimeout(BACKEND_TIMEOUT, TimeUnit.SECONDS)
+                .writeTimeout(BACKEND_TIMEOUT, TimeUnit.SECONDS)
                 .build();
     }
 }
